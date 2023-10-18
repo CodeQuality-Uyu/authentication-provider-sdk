00b5bc9e0579da0300ff24170f2e24444515428a
Daniel Acevedo <daniel18acevedo@gmail.com>
2023-10-18 11:56:40 -0300

    Adding documentation

--- CQ.AuthProvider.SDK/AppConfig/AuthSdkDependencyInjection.cs
+++ CQ.AuthProvider.SDK/AppConfig/AuthSdkDependencyInjection.cs
@@ -11,6 +11,17 @@ namespace CQ.AuthProvider.SDK.AppConfig
 {
     public static class AuthSdkDependencyInjection
     {
+        /// <summary>
+        /// Register the following necessary services:
+        /// 1. HttpClientAdapter -> Transient
+        /// 2. AuthService
+        /// 3. SessionService
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="cqAuthApiUrl"></param>
+        /// <param name="authServiceLifeTime"></param>
+        /// <param name="sessionServiceLifeTime"></param>
+        /// <returns></returns>
         public static IServiceCollection AddCqAuthService(
             this IServiceCollection services, 
             string cqAuthApiUrl, 
