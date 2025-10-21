80d9e81a0f0ccbd655ccfcf3295e572105f504df
Daniel Acevedo <daniel.acevedo@buildinglink.com>
2025-10-21 12:56:51 -0300

    Clean code

--- src/CQ.AuthProvider.SDK/Me/MeService.cs
+++ src/CQ.AuthProvider.SDK/Me/MeService.cs
@@ -3,12 +3,12 @@ using CQ.AuthProvider.SDK.Http;
 
 namespace CQ.AuthProvider.SDK.Me;
 
-internal sealed class MeService(AuthProviderConnectionApi _authProviderWebApi)
+internal sealed class MeService(AuthProviderConnectionApi authProviderWebApi)
 : IMeService
 {
     public async Task<AccountLogged> GetAsync(string token)
     {
-        var response = await _authProviderWebApi.GetAsync<AccountLogged>(
+        var response = await authProviderWebApi.GetAsync<AccountLogged>(
            $"me",
            [new("Authorization", token)])
            .ConfigureAwait(false);
