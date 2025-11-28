354961d8ed33969dd27e7adf04899c70af6b0714
chrisingrey <142596158+chrisingrey@users.noreply.github.com>
2025-11-28 11:27:11 -0300

    EC-492: AccountCreatedFor Interface

--- src/CQ.AuthProvider.SDK/Accounts/IAccountService.cs
+++ src/CQ.AuthProvider.SDK/Accounts/IAccountService.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel;
+
 namespace CQ.AuthProvider.SDK.Accounts;
 
 public interface IAccountService
 {
     Task<AccountCreated> CreateAsync(CreateAccountPasswordArgs args);
+    Task<LicenceeAccountCreated> CreateForAsync(CreateAccountForArgs args);
 }
