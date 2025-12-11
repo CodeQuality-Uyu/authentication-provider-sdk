474dd9b39ebb58719788c23f2dd021fa078773d9
Daniel Acevedo <daniel18acevedo@gmail.com>
2025-12-11 13:55:46 -0300

    Refactor blob

--- src/CQ.AuthProvider.SDK/Accounts/BlobRead.cs
+++ src/CQ.AuthProvider.SDK/Accounts/BlobRead.cs
@@ -3,9 +3,7 @@ namespace CQ.AuthProvider.SDK.Accounts;
 
 public readonly struct BlobRead()
 {
-    public Guid Id { get; init; }
-
     public string Key { get; init; } = null!;
 
-    public string ReadUrl { get; init; } = null!;
+    public string Url { get; init; } = null!;
 }
