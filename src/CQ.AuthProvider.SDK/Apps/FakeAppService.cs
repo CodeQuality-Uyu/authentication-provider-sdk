e0c52b26c82669d88bff9d973e9f5f5e1aa50522
Daniel Acevedo <daniel.acevedo@buildinglink.com>
2026-01-15 23:59:53 -0300

    Fixed build

--- src/CQ.AuthProvider.SDK/Apps/FakeAppService.cs
+++ src/CQ.AuthProvider.SDK/Apps/FakeAppService.cs
@@ -43,4 +43,34 @@ internal sealed class FakeAppService : IAppService
 
         return Task.FromResult(fakeApp);
     }
+
+    public Task<AppDetailedInfo> GetByIdAsync(Guid id, string subscriptionKey)
+    {
+         var fakeApp = new AppDetailedInfo
+        {
+            Id = id,
+            Name = "Fake App",
+            Logo = new Logo
+            {
+                Color = new BlobRead
+                {
+                    Url = "https://fake.com/logo-color.png",
+                    Key = "logo-color.png"
+                },
+                Light = new BlobRead
+                {
+                    Url = "https://fake.com/logo-light.png",
+                    Key = "logo-light.png"
+                },
+                Dark = new BlobRead
+                {
+                    Url = "https://fake.com/logo-dark.png",
+                    Key = "logo-dark.png"
+                }
+            },
+            Background = null
+        };
+
+        return Task.FromResult(fakeApp);
+    }
 }
